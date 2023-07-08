3871f174c275e5fededb9116a1deb38bc9ab0c13
Andrii F <smax.kh@gmail.com>
2023-07-08 12:27:17 +0300

    Format with prettier

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-function functionGo() { //скрыть блок iputInfo
-   var divInput = document.getElementsByClassName ('iputInfo');
-   var style = divInput[0].style;
-   style.display = "none";
-   console.log(divInput);
+function functionGo() {
+  //скрыть блок iputInfo
+  var divInput = document.getElementsByClassName("iputInfo");
+  var style = divInput[0].style;
+  style.display = "none";
+  console.log(divInput);
 
-   var divInput1 = document.getElementsByClassName ('group_Block_Calc');
-   var style1 = divInput1[0].style;
-   style1.display = "block";
-   console.log(divInput1);
+  var divInput1 = document.getElementsByClassName("group_Block_Calc");
+  var style1 = divInput1[0].style;
+  style1.display = "block";
+  console.log(divInput1);
 }
 window.onload = function () {
-   console.log("Page loaded");
- };
+  console.log("Page loaded");
+};
