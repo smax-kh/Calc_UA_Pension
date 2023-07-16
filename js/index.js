dc6b0419c338d1a2619a8249c0f4a58663ef7d31
Andrii F <smax.kh@gmail.com>
2023-07-16 09:30:00 +0300

    Minimaze code with one global variable

--- js/index.js
+++ js/index.js
@@ -2,30 +2,33 @@ let inputYearUser = 0;
 let inputNameUser = "";
 let inputEmailUser = "";
 let inputSex = 0;
+let inputArray;
+let messageOutput;
 function functionGo() {
   //скрыть блок iputInfo
-  var divInput = document.getElementsByClassName("iputInfo");
-  var style = divInput[0].style;
+  let divInput = document.getElementsByClassName("iputInfo");
+  let style = divInput[0].style;
   style.display = "none";
   console.log(divInput);
 
-  var divInput1 = document.getElementsByClassName("group_Block_Calc");
-  var style1 = divInput1[0].style;
+  let divInput1 = document.getElementsByClassName("group_Block_Calc");
+  let style1 = divInput1[0].style;
   style1.display = "block";
   console.log(divInput1);
 }
 window.onload = function () {
+  inputArray = document.querySelectorAll("input");
+  messageOutput = document.getElementById("massageError");
   yearCheck();
   emailCheck();
   nameParse();
   sexCheck();
+  blockButton();
 };
 
 function emailCheck() {
-  let emailUser = document.querySelectorAll("input");
-  let messageOutput = document.getElementById("massageError");
   let email_flag = false;
-  emailUser[4].onkeyup = function (event) {
+  inputArray[4].oninput = function (event) {
     let filter =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!filter.test(emailUser[4].value)) {
@@ -45,28 +48,26 @@ function emailCheck() {
       inputEmailUser = this.value;
       console.log(inputEmailUser);
     }
+    blockButton();
   };
 }
 
 function nameParse() {
-  let nameUser = document.querySelectorAll("input");
-  let messageOutput = document.getElementById("massageError");
-  nameUser[0].onkeyup = function (event) {
-    this.value = this.value.replace(/[^A-Za-zА-Яа-я]+/g, "");
-    if (this.value.length >= 1) {
+  inputArray[0].oninput = function (event) {
+    this.value = this.value.replace(/[^A-Za-zА-Яа-я ]+/g, "");
+    if (this.value.length > 0) {
       event.target.classList.remove("inputRed");
       messageOutput.innerText = "Enter your data...";
     } else {
       event.target.classList.add("inputRed");
       messageOutput.innerText = "Name must contain only letters!!!";
     }
+    blockButton();
   };
 }
 
 function yearCheck() {
-  let yearUserInput = document.querySelectorAll("input");
-  let messageOutput = document.getElementById("massageError");
-  yearUserInput[3].onkeyup = function (event) {
+  inputArray[3].oninput = function (event) {
     this.value = yearUserInput[3].value.replace(/\D/, "");
     let inputData = yearUserInput[3].value;
     let count = inputData.length;
@@ -83,6 +84,7 @@ function yearCheck() {
       event.target.classList.remove("inputRed");
       messageOutput.innerText = "Enter your data...";
     }
+    blockButton();
   };
 }
 
@@ -98,7 +100,17 @@ function sexCheck() {
           sexCheck = radio.value;
         }
       }
+      blockButton();
     },
     false
   );
+}
+
+function blockButton() {
+  let btn = document.getElementsByClassName("inputFildButton");
+  if (inputYearUser === 0 || inputNameUser === "" || inputEmailUser === "") {
+    // btn[0].disabled = true;
+  } else {
+    // btn[0].disabled = false;
+  }
 }
